42f74401eaa6c4d66194f9e731d2933c59b1a70c
harjeevansingh <harjeevansingh01@gmail.com>
2024-07-08 01:47:14 +0530

    fix: cors in local

--- src/main/java/com/historyservice/controllers/v1/HistoryController.java
+++ src/main/java/com/historyservice/controllers/v1/HistoryController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/history")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HistoryController {
 
     @Autowired
